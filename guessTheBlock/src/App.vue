e8d6b81f63915692ed7993874871dee9f33b7d16
rabgonzalez <rubalba.rag@gmail.com>
2025-02-10 19:55:53 +0000

    feat(mcGuessTheBlock): infraestructure defined

---
+++ guessTheBlock/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <section v-if="true">
+    <h1>Loading...</h1>
+  </section>
+  <section v-else>
+    <h1>¡Loaded!</h1>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
