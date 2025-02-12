4d06fc67a233bae8ce72d1bda3b9638c92bdeca5
JonayKB <jonaykb@gmail.com>
2025-02-12 22:46:51 +0000

    fix(GuessView): Added props to valeus

--- guessTheBlock/src/modules/views/GuessView.vue
+++ guessTheBlock/src/modules/views/GuessView.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <h1>{{ selectedBlock?.name }}</h1>
-    <h1>{{ selectedBlock?.displayName }}</h1>
-    <h1>{{ selectedBlock?.stackSize }}</h1>
-    <h1>{{ selectedBlock?.transparent }}</h1>
-    <h1>{{ selectedBlock?.emitLight }}</h1>
-    <h1>{{ selectedBlock?.crossable }}</h1>
-    <h1>{{ selectedBlock?.hardness }}</h1>
-    <h1>{{ selectedBlock?.toolLevel }}</h1>
-    <h1>{{ selectedBlock?.tool }}</h1>
-    <h1>{{ selectedBlock?.blastResistance }}</h1>
+    <h1>{{ props.selectedBlock?.name }}</h1>
+    <h1>{{ props.selectedBlock?.displayName }}</h1>
+    <h1>{{ props.selectedBlock?.stackSize }}</h1>
+    <h1>{{ props.selectedBlock?.transparent }}</h1>
+    <h1>{{ props.selectedBlock?.emitLight }}</h1>
+    <h1>{{ props.selectedBlock?.crossable }}</h1>
+    <h1>{{ props.selectedBlock?.hardness }}</h1>
+    <h1>{{ props.selectedBlock?.toolLevel }}</h1>
+    <h1>{{ props.selectedBlock?.tool }}</h1>
+    <h1>{{ props.selectedBlock?.blastResistance }}</h1>
   </div>
 </template>
 
