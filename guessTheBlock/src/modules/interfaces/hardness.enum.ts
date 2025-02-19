d197658b5dc5797136cc7017a5eae1d0c530e21a
JonayKB <jonaykb@gmail.com>
2025-02-19 15:01:41 +0000

    fix: Some fixes

--- guessTheBlock/src/modules/interfaces/hardness.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum hardness {
-  instantaneous = 0,
-  fast = 1,
-  normal = 3,
-  slow = 4.5,
-  very_slow = 5,
-  unbreakable = -1
-}
--- guessTheBlock/tailwind.config.js
+++ guessTheBlock/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist_electron/*/.{html,js,vue}", "./public/index.html", "./renderer/*/.{html,js,vue}", "./src/*/.{html,js,vue}"],
+  content: [
+    "./dist_electron/*/.{html,js,vue}",
+    "./public/index.html",
+    "./renderer/*/.{html,js,vue}",
+    "./src/*/.{html,js,vue}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
